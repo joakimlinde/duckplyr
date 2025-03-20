044323af0465a105262ce116535954d78d2ee4ee
Joakim Linde <joakim@lindelabs.org>
2025-03-20 21:31:33 +0300

    Add n_distinct() counts NA correctly

--- tests/testthat/test-n_distinct.R
+++ tests/testthat/test-n_distinct.R
@@ -9,7 +9,7 @@ test_that("n_distinct() basic", {
   out <- df |>
     summarise( n_distinct_x = n_distinct(x),
                n_distinct_x_na_rm = n_distinct(x, na.rm = TRUE),
-               n_distinct_y = n_distinct(y),
+               n_distinct_y = n_distinct(y, na.rm = FALSE),
                n_distinct_y_na_rm = n_distinct(y, na.rm = TRUE)
     )
 
@@ -17,4 +17,26 @@ test_that("n_distinct() basic", {
   expect_equal(out$n_distinct_x_na_rm, 2)
   expect_equal(out$n_distinct_y, 2)
   expect_equal(out$n_distinct_y_na_rm, 1)
+})
+
+
+test_that("n_distinct() counts NA correctly", {
+  withr::local_envvar(DUCKPLYR_FORCE = TRUE)
+
+  df <- duckdb_tibble(
+    x = c(1, NA, 1, NA, 2, NA, 2), 
+    y = c(3, 3, NA, 3, NA, 4, 5)
+  )
+
+  out <- df |>
+    summarise( n_distinct_x = n_distinct(x),
+               n_distinct_x_na_rm = n_distinct(x, na.rm = TRUE),
+               n_distinct_y = n_distinct(y, na.rm = FALSE),
+               n_distinct_y_na_rm = n_distinct(y, na.rm = TRUE)
+    )
+
+  expect_equal(out$n_distinct_x, 3)
+  expect_equal(out$n_distinct_x_na_rm, 2)
+  expect_equal(out$n_distinct_y, 4)
+  expect_equal(out$n_distinct_y_na_rm, 3)
 })
