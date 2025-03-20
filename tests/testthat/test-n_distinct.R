a93a60ac8ad77efdd81ec90f45260859db3d56cf
Joakim Linde <joakim@lindelabs.org>
2025-03-20 21:07:48 +0300

    Add basic test

---
+++ tests/testthat/test-n_distinct.R
@@ -0,0 +1,20 @@
+test_that("n_distinct() basic", {
+  withr::local_envvar(DUCKPLYR_FORCE = TRUE)
+
+  df <- duckdb_tibble(
+    x = c(1, 1, 2, 2, 2), 
+    y = c(3, 3, NA, 3, 3)
+  )
+
+  out <- df |>
+    summarise( n_distinct_x = n_distinct(x),
+               n_distinct_x_na_rm = n_distinct(x, na.rm = TRUE),
+               n_distinct_y = n_distinct(y),
+               n_distinct_y_na_rm = n_distinct(y, na.rm = TRUE)
+    )
+
+  expect_equal(out$n_distinct_x, 2)
+  expect_equal(out$n_distinct_x_na_rm, 2)
+  expect_equal(out$n_distinct_y, 2)
+  expect_equal(out$n_distinct_y_na_rm, 1)
+})
